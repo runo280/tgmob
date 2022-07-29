52651b76546a14f2892b9c88978b60791a3f05ce
runo280 <runo280@users.noreply.github.com>
2022-07-29 07:48:30 +0430

    update

--- db.py
+++ db.py
@@ -2,7 +2,7 @@
 import os
 import pymongo
 
-db_url = 'mongodb+srv://{user}:{pwd}@{murl}/test?retryWrites=true&w=majority'
+db_url = 'mongodb://{user}:{pwd}@{murl}/?ssl=true&replicaSet=Main0-shard-0&authSource=admin&retryWrites=true&w=majority'
 db_user = os.environ['muser']
 db_pass = os.environ['mpass']
 db_domain = os.environ['murl']
@@ -10,7 +10,6 @@ db_name = 'mobilism'
 db_posts = 'posts'
 db_url = db_url.format(user=db_user, pwd=db_pass, murl=db_domain)
 client = pymongo.MongoClient(db_url)
-
 database = client[db_name]
 post_collection = database['posts']
 
